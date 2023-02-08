6cc9890b904f885d10f2f6ebbc17b7cf072b361c
SouzaPatrick <souza.felipe.patrick@gmail.com>
2023-02-08 19:46:48 -0300

    fix: Fix validation when token is invalid

--- tools/mais_todos.py
+++ tools/mais_todos.py
@@ -7,7 +7,10 @@ from tools.person_docs_helper import remove_mask_cpf
 
 
 def send_cashback(cashback_value: float, document: str, current_user: User) -> dict:
-    payload = {"document": remove_mask_cpf(document), "cashback": cashback_value}
+    payload = {
+        "document": remove_mask_cpf(document),
+        "cashback": str(int(cashback_value)),
+    }
     url = "https://5efb30ac80d8170016f7613d.mockapi.io/api/mock/Cashback"
     headers: dict = {
         "Content-Type": "application/json;charset=UTF-8",
